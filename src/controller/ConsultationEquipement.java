545b36e623affa862130e03dad13516845f40c9f
mfiari <fiarimike@yahoo.fr>
2015-04-28 09:17:45 +0200

    affichage des équipement

--- src/controller/ConsultationEquipement.java
+++ src/controller/ConsultationEquipement.java
@@ -1,9 +1,13 @@
 package controller;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
+import application.DataTest;
 import model.Equipement;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
@@ -13,9 +17,15 @@ public class ConsultationEquipement implements Initializable{
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
-		
+		DataTest dataTest = new DataTest();
+		List<Equipement> list = dataTest.getEquipements();
+		ObservableList<String> items = FXCollections.observableArrayList();
+		for (Equipement equip : list) {
+			items.add(equip.toString());
+		}
+		lstEquips.setItems(items);
 	}
 
 	@FXML
-	private ListView<Equipement> lstEquips;
+	private ListView<String> lstEquips;
 }
