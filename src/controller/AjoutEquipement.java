e101a4fb7962693c11e053b6d7cb2eab61d12de0
cfarot <charlyfarot@gmail.com>
2015-05-13 00:59:18 +0200

    clean warning

--- src/controller/AjoutEquipement.java
+++ src/controller/AjoutEquipement.java
@@ -3,20 +3,17 @@ package controller;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-
-import tools.ManipInterface;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
 import model.Logiciel;
+import tools.ManipInterface;
 
 public class AjoutEquipement implements Initializable{
 
