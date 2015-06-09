3465fa6edceda943b389e499bf487f1c3584e9c7
mfiari <fiarimike@yahoo.fr>
2015-06-09 19:24:07 +0200

    ajout javadoc class Pole

--- src/model/Pole.java
+++ src/model/Pole.java
@@ -4,6 +4,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+/**
+ * Pole est la classe représentant un pôle.
+ * 
+ * @version 1.0
+ */
 @Entity
 public class Pole {
 
@@ -13,24 +18,55 @@ public class Pole {
 	
 	private String nom;
 	
-	
+	/**
+	 * Constructeur par défaut
+	 */
 	public Pole(){}
+	
+	/**
+	 * Constructeur pole
+	 *
+	 * @param nom
+	 *     nom du pôle
+	 */
 	public Pole(String nom) {
 		this.nom = nom;
 	}
 	
+	/**
+     * Retourne l'identifiant du pôle.
+     * 
+     * @return un entier correspondant à l'identifiant du pôle.
+    */
 	public int getId () {
 		return this.idPole;
 	}
 
+	/**
+     * Retourne le nom du pôle.
+     * 
+     * @return une chaine de caractère correspondant au nom du pôle.
+    */
 	public String getNom() {
 		return nom;
 	}
 
+	/**
+	* Affecte un nouveau nom au pôle
+	*
+	* @param nom
+	*     nom du pôle
+	*     
+	*/
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
 	
+	/**
+     * Retourne un pôle sous forme de chaine.
+     * 
+     * @return une chaine de caractère correspondant au pôle.
+     */
 	@Override
 	public String toString() {
 		return nom;
