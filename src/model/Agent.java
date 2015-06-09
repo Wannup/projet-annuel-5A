c774e6afd8209bf4c711418e48baafdbd0fcaa44
mfiari <fiarimike@yahoo.fr>
2015-06-09 16:16:30 +0200

    ajout javadoc class Agent

--- src/model/Agent.java
+++ src/model/Agent.java
@@ -1,7 +1,5 @@
 package model;
 
-import javafx.stage.Stage;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -34,7 +32,15 @@ public class Agent {
 	 *
 	 * @param nom
 	 *     nom de l'agent
-	 * @see Stage
+	 * @param prenom
+	 *     prenom de l'agent
+	 * @param dateDeNaissance
+	 *     date de naissance de l'agent
+	 * @param numCP
+	 *     numéro de l'agent
+	 * @param numPoste
+	 *     numéro de poste de l'agent
+	 *     
 	 */
 	public Agent(String nom, String prenom, String dateDeNaissance, String numCP, String numPoste) {
 		
@@ -45,50 +51,120 @@ public class Agent {
 		this.numPoste = numPoste;
 	}
 	
+	/**
+     * Retourne l'identifiant de l'agent.
+     * 
+     * @return un entier correspondant à l'identifiant de l'agent.
+     */
 	public int getId () {
 		return this.idAgent;
 	}
 
+	/**
+     * Retourne le nom de l'agent.
+     * 
+     * @return une chaine de caractère correspondant au nom de l'agent.
+     */
 	public String getNom() {
 		return nom;
 	}
 
+	/**
+	* Affecte un nouveau nom à l'agent
+	*
+	* @param nom
+	*     nom de l'agent
+	*     
+	*/
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
 
+	/**
+     * Retourne le prénom de l'agent.
+     * 
+     * @return une chaine de caractère correspondant au prénom de l'agent.
+     */
 	public String getPrenom() {
 		return prenom;
 	}
 
+	/**
+	* Affecte un nouveau prénom à l'agent
+	*
+	* @param prenom
+	*     prénom de l'agent
+	*     
+	*/
 	public void setPrenom(String prenom) {
 		this.prenom = prenom;
 	}
 
+	/**
+     * Retourne le nom de l'agent.
+     * 
+     * @return une chaine de caractère correspondant au nom de l'agent.
+     */
 	public String getDateDeNaissance() {
 		return dateDeNaissance;
 	}
 
+	/**
+	* Affecte une nouvelle date de naissance à l'agent
+	*
+	* @param dateDeNaissance
+	*     date de naissance de l'agent
+	*     
+	*/
 	public void setDateDeNaissance(String dateDeNaissance) {
 		this.dateDeNaissance = dateDeNaissance;
 	}
 
+	/**
+     * Retourne le numéro del'agent.
+     * 
+     * @return une chaine de caractère correspondant au numéro de l'agent.
+     */
 	public String getNumCP() {
 		return numCP;
 	}
 
+	/**
+	* Affecte un nouveau numéro à l'agent
+	*
+	* @param numCP
+	*     numéro de l'agent
+	*     
+	*/
 	public void setNumCP(String numCP) {
 		this.numCP = numCP;
 	}
 
+	/**
+     * Retourne le numéro de poste de l'agent.
+     * 
+     * @return une chaine de caractère correspondant au numéro de poste de l'agent.
+     */
 	public String getNumPoste() {
 		return numPoste;
 	}
 
+	/**
+	* Affecte un nouveau numéro de poste à l'agent
+	*
+	* @param numPoste
+	*     numéro de poste de l'agent
+	*     
+	*/
 	public void setNumPoste(String numPoste) {
 		this.numPoste = numPoste;
 	}
 	
+	/**
+     * Retourne un agent sous forme de chaine.
+     * 
+     * @return une chaine de caractère correspondant à l'agent.
+     */
 	@Override
 	public String toString() {
 		return "" + nom + " " + prenom;
