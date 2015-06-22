635e09f4141e6489e31d49c345d5c24356c82244
cfarot <charlyfarot@gmail.com>
2015-06-23 00:31:36 +0200

    graph par type with real data

--- src/controller/GraphType.java
+++ src/controller/GraphType.java
@@ -4,34 +4,43 @@ import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
-import model.Equipement;
-import model.TypeEquipement;
-import dao.EquipementDao;
-import dao.TypeEquipementDao;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.PieChart;
 import javafx.scene.chart.PieChart.Data;
+import model.TypeEquipement;
+import dao.EquipementDao;
+import dao.TypeEquipementDao;
 
 public class GraphType implements Initializable{
 	
 	@FXML
 	private PieChart typePchart;
 	
+	private TypeEquipementDao typeEquipementDao;
+	private EquipementDao equipementDao;
+	
+	private List<TypeEquipement> lstTypeEquipement;
 
 	@Override
-	public void initialize(URL location, ResourceBundle resources) {		
+	public void initialize(URL location, ResourceBundle resources) {
+		this.typeEquipementDao = new TypeEquipementDao();
+		this.equipementDao = new EquipementDao();
+		this.lstTypeEquipement = typeEquipementDao.findByAttributesLike(null);
         this.typePchart.setData(getChartData());
 	}
 	
 	private ObservableList<Data> getChartData() {
+		
 		ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
-
-		pieChartData.add(new PieChart.Data("PC Fixe", 3));
-		pieChartData.add(new PieChart.Data("PC Portable", 10));
-		pieChartData.add(new PieChart.Data("Imprimante", 5));
+		
+		for(int i = 0; i< this.lstTypeEquipement.size(); i++){
+			String nomType = this.lstTypeEquipement.get(i).getNom();
+			int count = equipementDao.getEquipementByType(this.lstTypeEquipement.get(i)).size();
+			pieChartData.add(new PieChart.Data(nomType, count));
+		}
 
         return pieChartData;
     }
