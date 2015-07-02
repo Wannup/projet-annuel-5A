6399cf49b7c050fccd2822c4a3384adbb31b69b6
mfiari <fiarimike@yahoo.fr>
2015-07-03 00:53:29 +0200

    export graphique en png

--- src/controller/GraphType.java
+++ src/controller/GraphType.java
@@ -1,15 +1,26 @@
 package controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
+import javax.imageio.ImageIO;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.chart.PieChart;
 import javafx.scene.chart.PieChart.Data;
+import javafx.scene.image.WritableImage;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 import model.TypeEquipement;
 import dao.EquipementDao;
 import dao.TypeEquipementDao;
@@ -18,6 +29,9 @@ import dao.TypeEquipementDao;
  * class controller for the interface GraphType.fxml
  * */
 public class GraphType implements Initializable{
+
+	@FXML
+	private AnchorPane bodyPanel;
 	
 	@FXML
 	private PieChart typePchart;
@@ -47,5 +61,22 @@ public class GraphType implements Initializable{
 
         return pieChartData;
     }
+	
+	@FXML
+	private void exportGraph(ActionEvent event) throws IOException {
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.getExtensionFilters().add(new ExtensionFilter("PNG", "*.png"));
+		fileChooser.setInitialFileName("type d'equipement");
+        fileChooser.setTitle("Enregister graphique");
+        File file;
+        file = fileChooser.showSaveDialog(bodyPanel.getParent().getScene().getWindow());
+        if (file != null) {
+        	PieChart chart = new PieChart(getChartData());
+            Scene scene = new Scene(chart);
+            WritableImage snapShot = scene.snapshot(null);
+            ImageIO.write(SwingFXUtils.fromFXImage(snapShot, null), "png", file);
+        }
+		
+	}
 
 }
