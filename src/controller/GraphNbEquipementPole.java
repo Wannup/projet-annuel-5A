32ec5a357f4d9f9361f79e0b32f2b3ffc178d1c5
mfiari <fiarimike@yahoo.fr>
2015-07-07 18:04:02 +0200

    export graph

--- src/controller/GraphNbEquipementPole.java
+++ src/controller/GraphNbEquipementPole.java
@@ -1,18 +1,29 @@
 package controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
+import javax.imageio.ImageIO;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Side;
+import javafx.scene.Scene;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.StackedBarChart;
 import javafx.scene.chart.XYChart;
+import javafx.scene.image.WritableImage;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 import model.Agent;
 import model.Pole;
 import model.TypeEquipement;
@@ -28,6 +39,9 @@ import dao.TypeEquipementDao;
  * @version 1.0
  */
 public class GraphNbEquipementPole implements Initializable{
+	
+	@FXML
+	private AnchorPane bodyPanel;
 	
 	@FXML
 	private StackedBarChart SBchart;
@@ -67,6 +81,42 @@ public class GraphNbEquipementPole implements Initializable{
         yAxis.setLabel("Quantité");
         
         this.SBchart.setData(s);			
+	}
+	
+	@FXML
+	private void exportGraph() throws IOException {
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.getExtensionFilters().add(new ExtensionFilter("PNG", "*.png"));
+		fileChooser.setInitialFileName("Nombre d'équipement par type");
+        fileChooser.setTitle("Enregister graphique");
+        File file;
+        file = fileChooser.showSaveDialog(bodyPanel.getParent().getScene().getWindow());
+        if (file != null) {
+        	CategoryAxis xAxis = new CategoryAxis();
+        	xAxis.setLabel(SBchart.getXAxis().getLabel());
+        	xAxis.setCategories(FXCollections.<String>observableArrayList(((CategoryAxis)SBchart.getXAxis()).getCategories()));
+        	xAxis.setSide(Side.BOTTOM);
+        	NumberAxis yAxis = new NumberAxis();
+        	yAxis.setLabel(SBchart.getYAxis().getLabel());
+        	yAxis.setSide(Side.LEFT);
+        	StackedBarChart<String, Number> chart = new StackedBarChart<>(xAxis, yAxis);
+        	
+        	ObservableList<XYChart.Series<String, Number>> observableList = FXCollections.observableArrayList(SBchart.getData());
+        	ObservableList<XYChart.Series<String, Number>> observableList2 = FXCollections.observableArrayList();
+        	for (XYChart.Series<String, Number> series : observableList) {
+        		XYChart.Series<String, Number> type = new XYChart.Series<String, Number>();
+    			type.setName(series.getName());
+    			ObservableList<XYChart.Data<String, Number>> datas = FXCollections.observableArrayList(series.getData());
+    			for (XYChart.Data<String, Number> data : datas) {
+    				type.getData().add(new XYChart.Data<String, Number>(data.getXValue(), data.getYValue()));
+    			}			
+    			observableList2.add(type);
+        	}
+        	chart.setData(observableList2);
+            Scene scene = new Scene(chart);
+            WritableImage snapShot = scene.snapshot(null);
+            ImageIO.write(SwingFXUtils.fromFXImage(snapShot, null), "png", file);
+        }
 	}
     
    /* private AgentDao agentDao;
--- src/controller/GraphRenewal.java
+++ src/controller/GraphRenewal.java
@@ -1,20 +1,33 @@
 package controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 import java.util.ResourceBundle;
 
+import javax.imageio.ImageIO;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Side;
+import javafx.scene.Scene;
+import javafx.scene.chart.Axis;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.PieChart;
 import javafx.scene.chart.StackedBarChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.ComboBox;
+import javafx.scene.image.WritableImage;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 import model.Equipement;
 import model.TypeEquipement;
 import dao.EquipementDao;
@@ -26,6 +39,9 @@ import dao.TypeEquipementDao;
  * @version 1.0
  */
 public class GraphRenewal implements Initializable{
+	
+	@FXML
+	private AnchorPane bodyPanel;
 
 	@FXML
 	private StackedBarChart renewalSBchart;
@@ -149,5 +165,41 @@ public class GraphRenewal implements Initializable{
 				displayByQuantity();
 			}
 		}
+	}
+	
+	@FXML
+	private void exportGraph() throws IOException {
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.getExtensionFilters().add(new ExtensionFilter("PNG", "*.png"));
+		fileChooser.setInitialFileName("renouvellement (par type)");
+        fileChooser.setTitle("Enregister graphique");
+        File file;
+        file = fileChooser.showSaveDialog(bodyPanel.getParent().getScene().getWindow());
+        if (file != null) {
+        	CategoryAxis xAxis = new CategoryAxis();
+        	xAxis.setLabel(renewalSBchart.getXAxis().getLabel());
+        	xAxis.setCategories(FXCollections.<String>observableArrayList(((CategoryAxis)renewalSBchart.getXAxis()).getCategories()));
+        	xAxis.setSide(Side.BOTTOM);
+        	NumberAxis yAxis = new NumberAxis();
+        	yAxis.setLabel(renewalSBchart.getYAxis().getLabel());
+        	yAxis.setSide(Side.LEFT);
+        	StackedBarChart<String, Number> chart = new StackedBarChart<>(xAxis, yAxis);
+        	
+        	ObservableList<XYChart.Series<String, Number>> observableList = FXCollections.observableArrayList(renewalSBchart.getData());
+        	ObservableList<XYChart.Series<String, Number>> observableList2 = FXCollections.observableArrayList();
+        	for (XYChart.Series<String, Number> series : observableList) {
+        		XYChart.Series<String, Number> type = new XYChart.Series<String, Number>();
+    			type.setName(series.getName());
+    			ObservableList<XYChart.Data<String, Number>> datas = FXCollections.observableArrayList(series.getData());
+    			for (XYChart.Data<String, Number> data : datas) {
+    				type.getData().add(new XYChart.Data<String, Number>(data.getXValue(), data.getYValue()));
+    			}			
+    			observableList2.add(type);
+        	}
+        	chart.setData(observableList2);
+            Scene scene = new Scene(chart);
+            WritableImage snapShot = scene.snapshot(null);
+            ImageIO.write(SwingFXUtils.fromFXImage(snapShot, null), "png", file);
+        }
 	}
 }
